9267d6c9f941f9fa61f00e68819e4d807e95fed8
Gabriel Henrique Faustini Gomes <gabrielhfgomes@hotmail.com>
2026-01-09 09:07:16 -0300

    Initial commit

---
+++ src/main/java/com/gabriel/qaphptranslationagent/QaPhpTranslationAgentApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.qaphptranslationagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaPhpTranslationAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaPhpTranslationAgentApplication.class, args);
+	}
+
+}
